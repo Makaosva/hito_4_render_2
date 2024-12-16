956b4dc6456649689836cd56f1374e81456d6a53
Makaosva <maka.osses@gmail.com>
2024-12-16 01:21:49 -0300

    cambio en el cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const morgan = require("morgan");
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  //origin: "http://localhost:5173",
   credentials: true,
   exposedHeaders: ["Authorization"],
 };
